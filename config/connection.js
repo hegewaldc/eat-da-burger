9dd9bce8dffcd1ff1e69cb0316cb2b2055e59d8b
Chris Hegewald <hegewaldc@gmail.com>
2017-12-07 18:34:17 -0500

    updates

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,9 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
-        root: 3000,
         host: 'localhost',
-        user: 'root',
-        password: '',
+        user: 'chrish',
+        password: 'allyourbase',
         database: 'burgers_db',
     });
 };
